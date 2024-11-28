87ec7868ac3de26267afab300c0373e9c58d1fff
Koyeb-LK <dilishadilisha29@gmail.com>
2024-11-28 13:42:15 +0530

    🧚‍♂️⃝𝚀𝚄𝙴𝙴𝙽_𝙰𝙷𝙸𝙽𝚂𝙰-𝙼𝙳 𝚅3💕

---
+++ app.js
@@ -0,0 +1,31 @@
+{
+  "name": "QUEEN-AHINSA-MD_V3",
+  "description": "Javascript WhatsApp bot made by Dilisha Gimshan",
+  "logo": "https://i.ibb.co/SR76mBh/Pu3-ZYHBS5139.jpg",
+  "keywords": ["bot"],
+  "success_url": "/",
+
+  "env": {
+    "SESSION_ID": {
+      "description": "Put the session-id here.",
+      "required": true
+    },  
+
+    "ALIVE_IMG": {
+      "description": "Put your alive img here.",
+      "required": true
+    },
+    
+    "ALIVE_MSG": {
+      "description": "Put your alive msg here.",
+      "required": true
+    }   
+},
+
+     "buildpacks": [
+        {
+            "url": "https://github.com/heroku/heroku-buildpack-nodejs.git"
+        }
+     ],
+  "stack": "heroku-24"
+}
